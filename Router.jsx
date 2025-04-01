790733f10dca5647a353afa7cba77a4fa1f9eb5e
NatiF <65537815+OptimaLPro@users.noreply.github.com>
2025-04-01 20:51:52 +0300

    Add login page, implement form validation, and enhance project details display

--- Router.jsx
+++ Router.jsx
@@ -3,13 +3,12 @@ import Instructors from "@/pages/Instructors/Instructors";
 import Project from "@/pages/Project/Project";
 import React, { useEffect } from "react";
 import { Route, Routes } from "react-router";
+import { Login } from "@/pages/Login/Login";
 
 const Router = () => {
-
   useEffect(() => {
     document.documentElement.scrollTo(0, 0);
-  }
-  , []);
+  }, []);
 
   return (
     <div className="pt-5 lg:py-12">
@@ -17,6 +16,7 @@ const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/projects/:id" element={<Project />} />
         <Route path="/instructors" element={<Instructors />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </div>
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,95 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+});
+
+export function Login() {
+  // 1. Define your form.
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <div className="relative mx-auto w-[80%] max-w-[300px] mt-16">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-lg">Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Enter your email"
+                    {...field}
+                    className="bg-white"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-lg">Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter your password"
+                    {...field}
+                    className="bg-white"
+                  />
+                </FormControl>
+                {/* <FormDescription>
+                                    Choose a strong password to keep your account secure.
+                                </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex items-center justify-center mt-12">
+            <Button type="submit" className="text-lg">
+              Submit
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
