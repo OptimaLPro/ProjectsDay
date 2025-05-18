9f60763ace3de202b5c1aae793805cdd3d274583
NatiF <65537815+OptimaLPro@users.noreply.github.com>
2025-05-18 18:30:14 +0300

    feat: update contributor link and refactor image rendering in Footer component; replace direct link with React Router Link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,13 +6,15 @@ import {
   TooltipTrigger,
 } from "./tooltip_footer";
 import { Button } from "../ui/button";
+import { Link } from "react-router";
 
 const ContributorsData = [
   {
     name: "Nati Forish",
+    id: "68176b867c9b5972730731c6",
     image:
       "https://res.cloudinary.com/dwgbi63rq/image/upload/v1747240065/Me_-_Copy_nlkvtt.jpg",
-    link: "https://github.com/OptimaLPro",
+    link: "https://graduationday.vercel.app/users/68176b867c9b5972730731c6",
   },
 ];
 
@@ -21,7 +23,12 @@ const Footer = () => {
     <footer className="py-4 text-zinc-700">
       <div className="container mx-auto text-center">
         <div className="flex items-center justify-center gap-2 mb-2">
-          <img className="h-4 opacity-80" src={"https://res.cloudinary.com/dwgbi63rq/image/upload/v1747258721/ColmanDevClub_Favicon_olici1.png"} />
+          <img
+            className="h-4 opacity-80"
+            src={
+              "https://res.cloudinary.com/dwgbi63rq/image/upload/v1747258721/ColmanDevClub_Favicon_olici1.png"
+            }
+          />
           <p>
             <a
               href="https://colmandevclub.vercel.app/"
@@ -29,7 +36,7 @@ const Footer = () => {
               rel="noopener noreferrer"
             >
               {/* {new Date().getFullYear()} */}
-               ColmanDevClub
+              ColmanDevClub
             </a>
           </p>
         </div>
@@ -41,19 +48,21 @@ const Footer = () => {
                 <Tooltip>
                   <TooltipTrigger>
                     <Button variant="link" className="p-0">
-                      <img
-                        className="w-6 h-6 rounded-full border-[1px] border-zinc-500"
-                        src={contributor.image}
-                        alt={contributor.name}
-                        onClick={() =>
-                          window.open(
-                            contributor.link,
-                            "_blank",
-                            "noopener,noreferrer"
-                          )
-                        }
-                        style={{ cursor: "pointer" }}
-                      />
+                      <Link to={`/users/${contributor.id}`}>
+                        <img
+                          className="w-6 h-6 rounded-full border-[1px] border-zinc-500"
+                          src={contributor.image}
+                          alt={contributor.name}
+                          // onClick={() =>
+                          //   window.open(
+                          //     contributor.link,
+                          //     "_blank",
+                          //     "noopener,noreferrer"
+                          //   )
+                          // }
+                          style={{ cursor: "pointer" }}
+                        />
+                      </Link>
                     </Button>
                   </TooltipTrigger>
                   <TooltipContent>{contributor.name}</TooltipContent>
